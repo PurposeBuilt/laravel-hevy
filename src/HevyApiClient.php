0ce7452bc21d39c5be7ec69443dea8494509ee65
Scott Yewell <scott@purposebuiltsoftware.com>
2025-01-24 18:11:19 -0500

    updated endpoints and base_uri

--- src/HevyApiClient.php
+++ src/HevyApiClient.php
@@ -11,7 +11,7 @@ class HevyApiClient
     public function __construct($apiKey)
     {
         $this->client = new Client([
-            'base_uri' => 'https://api.hevyapp.com/v1',
+            'base_uri' => 'https://api.hevyapp.com/v1/',
             'headers' => [
                 'api-key' => $apiKey,
                 'Accept' => 'application/json',
@@ -40,13 +40,13 @@ class HevyApiClient
     // User Endpoints
     public function getUser()
     {
-        return $this->request('GET', '/user');
+        return $this->request('GET', 'user');
     }
 
     // Workout Endpoints
     public function getWorkouts($page = 1, $pageSize = 5)
     {
-        return $this->request('GET', '/workouts', [
+        return $this->request('GET', 'workouts', [
             'page' => $page,
             'pageSize' => $pageSize,
         ]);
@@ -54,95 +54,95 @@ class HevyApiClient
 
     public function getWorkoutById($id)
     {
-        return $this->request('GET', "/workouts/$id");
+        return $this->request('GET', "workouts/$id");
     }
 
     public function createWorkout(array $data)
     {
-        return $this->request('POST', '/workouts', $data);
+        return $this->request('POST', 'workouts', $data);
     }
 
     public function updateWorkout($id, array $data)
     {
-        return $this->request('PUT', "/workouts/$id", $data);
+        return $this->request('PUT', "workouts/$id", $data);
     }
 
     public function deleteWorkout($id)
     {
-        return $this->request('DELETE', "/workouts/$id");
+        return $this->request('DELETE', "workouts/$id");
     }
 
     // Exercise Endpoints
     public function getExercises()
     {
-        return $this->request('GET', '/exercises');
+        return $this->request('GET', 'exercises');
     }
 
     public function getExerciseById($id)
     {
-        return $this->request('GET', "/exercises/$id");
+        return $this->request('GET', "exercises/$id");
     }
 
     public function createExercise(array $data)
     {
-        return $this->request('POST', '/exercises', $data);
+        return $this->request('POST', 'exercises', $data);
     }
 
     public function updateExercise($id, array $data)
     {
-        return $this->request('PUT', "/exercises/$id", $data);
+        return $this->request('PUT', "exercises/$id", $data);
     }
 
     public function deleteExercise($id)
     {
-        return $this->request('DELETE', "/exercises/$id");
+        return $this->request('DELETE', "exercises/$id");
     }
 
     // Set Endpoints
     public function getSets($workoutId)
     {
-        return $this->request('GET', "/workouts/$workoutId/sets");
+        return $this->request('GET', "workouts/$workoutId/sets");
     }
 
     public function createSet($workoutId, array $data)
     {
-        return $this->request('POST', "/workouts/$workoutId/sets", $data);
+        return $this->request('POST', "workouts/$workoutId/sets", $data);
     }
 
     public function updateSet($workoutId, $setId, array $data)
     {
-        return $this->request('PUT', "/workouts/$workoutId/sets/$setId", $data);
+        return $this->request('PUT', "workouts/$workoutId/sets/$setId", $data);
     }
 
     public function deleteSet($workoutId, $setId)
     {
-        return $this->request('DELETE', "/workouts/$workoutId/sets/$setId");
+        return $this->request('DELETE', "workouts/$workoutId/sets/$setId");
     }
 
     // Measurement Endpoints
     public function getMeasurements()
     {
-        return $this->request('GET', '/measurements');
+        return $this->request('GET', 'measurements');
     }
 
     public function getMeasurementById($id)
     {
-        return $this->request('GET', "/measurements/$id");
+        return $this->request('GET', "measurements/$id");
     }
 
     public function createMeasurement(array $data)
     {
-        return $this->request('POST', '/measurements', $data);
+        return $this->request('POST', 'measurements', $data);
     }
 
     public function updateMeasurement($id, array $data)
     {
-        return $this->request('PUT', "/measurements/$id", $data);
+        return $this->request('PUT', "measurements/$id", $data);
     }
 
     public function deleteMeasurement($id)
     {
-        return $this->request('DELETE', "/measurements/$id");
+        return $this->request('DELETE', "measurements/$id");
     }
 
 }
