bf57b21c72c0a527959c9be9309034a6c1b36fc9
Scott Yewell <scott@purposebuiltsoftware.com>
2025-01-27 13:16:27 -0500

    cleaned up available queries

--- src/HevyApiClient.php
+++ src/HevyApiClient.php
@@ -37,12 +37,6 @@ class HevyApiClient
     }
 
 
-    // User Endpoints
-    public function getUser()
-    {
-        return $this->request('GET', 'user');
-    }
-
     // Workout Endpoints
     public function getWorkouts($page = 1, $pageSize = 5)
     {
@@ -55,94 +49,6 @@ class HevyApiClient
     public function getWorkoutById($id)
     {
         return $this->request('GET', "workouts/$id");
-    }
-
-    public function createWorkout(array $data)
-    {
-        return $this->request('POST', 'workouts', $data);
-    }
-
-    public function updateWorkout($id, array $data)
-    {
-        return $this->request('PUT', "workouts/$id", $data);
-    }
-
-    public function deleteWorkout($id)
-    {
-        return $this->request('DELETE', "workouts/$id");
-    }
-
-    // Exercise Endpoints
-    public function getExercises()
-    {
-        return $this->request('GET', 'exercises');
-    }
-
-    public function getExerciseById($id)
-    {
-        return $this->request('GET', "exercises/$id");
-    }
-
-    public function createExercise(array $data)
-    {
-        return $this->request('POST', 'exercises', $data);
-    }
-
-    public function updateExercise($id, array $data)
-    {
-        return $this->request('PUT', "exercises/$id", $data);
-    }
-
-    public function deleteExercise($id)
-    {
-        return $this->request('DELETE', "exercises/$id");
-    }
-
-    // Set Endpoints
-    public function getSets($workoutId)
-    {
-        return $this->request('GET', "workouts/$workoutId/sets");
-    }
-
-    public function createSet($workoutId, array $data)
-    {
-        return $this->request('POST', "workouts/$workoutId/sets", $data);
-    }
-
-    public function updateSet($workoutId, $setId, array $data)
-    {
-        return $this->request('PUT', "workouts/$workoutId/sets/$setId", $data);
-    }
-
-    public function deleteSet($workoutId, $setId)
-    {
-        return $this->request('DELETE', "workouts/$workoutId/sets/$setId");
-    }
-
-    // Measurement Endpoints
-    public function getMeasurements()
-    {
-        return $this->request('GET', 'measurements');
-    }
-
-    public function getMeasurementById($id)
-    {
-        return $this->request('GET', "measurements/$id");
-    }
-
-    public function createMeasurement(array $data)
-    {
-        return $this->request('POST', 'measurements', $data);
-    }
-
-    public function updateMeasurement($id, array $data)
-    {
-        return $this->request('PUT', "measurements/$id", $data);
-    }
-
-    public function deleteMeasurement($id)
-    {
-        return $this->request('DELETE', "measurements/$id");
     }
 
     /**
@@ -167,6 +73,30 @@ class HevyApiClient
 
         return $this->request('GET', 'workouts/events', $params);
     }
+    
+    public function createWorkout(array $data)
+    {
+        return $this->request('POST', 'workouts', $data);
+    }
+
+    public function updateWorkout($id, array $data)
+    {
+        return $this->request('PUT', "workouts/$id", $data);
+    }
+
+
+    // Exercise Endpoints
+    public function getExercises()
+    {
+        return $this->request('GET', 'exercise_templates');
+    }
+
+    public function getExerciseById($id)
+    {
+        return $this->request('GET', "exercise_templates/$id");
+    }
+
+
     
 }
 
